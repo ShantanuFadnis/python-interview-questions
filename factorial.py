b4640c156187396a4a571c4fffef33d763968f2a
Shantanu Fadnis <shantanufadnis@Shantanus-MacBook-Pro.local>
2020-01-29 17:39:15 +0200

    Added 3 ps

--- factorial.py
+++ factorial.py
@@ -1,11 +1,9 @@
 def fact(n):
     if n == 0 or n == 1:
         return 1
-    
     res = 1
     for i in range(1, n + 1):
         res *= i
-    
     return res
 
 if __name__ == '__main__':
--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,6 @@
 def fib_recursive(n):
     if n <= 2:
         return 1
-
     return fib_recursive(n - 2) + fib_recursive(n - 1)
 
 
@@ -9,26 +8,21 @@ def fib_arr(n):
     temp = [0] * (n + 1)
     temp[0] = 1
     temp[1] = 1
-    
     for i in range(2, n):
         temp[i] = temp[i - 1] + temp[i - 2]
-    
     return temp[n - 1]
 
 
 def fib_memoization(n):
     if n <= 2:
         return 1
-    
     first = 1
     second = 1
     res = 0
-
     for _ in range(2, n):
         res = first + second
         first = second
         second = res
-
     return res
 
 
